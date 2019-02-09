94b06b5171e0157cb19193eab2995b951e7dd7be
Sumit Munshi <46409920+smunshi@users.noreply.github.com>
2019-02-09 02:52:52 -0500

    version 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fd:
 
 setuptools.setup(
     name="lair3",
-    version="0.1.3",
+    version="0.1.4",
     author="The Munshi Group",
     author_email="support@munshigroup.com",
     description="AmmuNation Networking Library",
