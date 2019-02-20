e4663cc3915af69c28cedf353db1650a70f0d455
Sumit Munshi <46409920+smunshi@users.noreply.github.com>
2019-02-20 15:27:42 -0500

    starting python3 migration

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fd:
 
 setuptools.setup(
     name="lair3",
-    version="0.1.10",
+    version="0.2",
     author="The Munshi Group",
     author_email="support@munshigroup.com",
     description="AmmuNation Networking Library",
@@ -15,6 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires = ['amncore', 'bombfuse', 'bs4', 'kthread', 'paynspray', 'pyopenssl'],
     classifiers=[
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
